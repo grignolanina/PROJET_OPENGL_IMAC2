338eec60984ad081a4f8c1a2381fe2b23209e0a9
grignolanina <grignola.nina@orange.fr>
2023-05-22 16:24:20 +0200

    prob conflic camera ok

--- src/camera.cpp
+++ src/camera.cpp
@@ -6,12 +6,6 @@
 #include "glm/trigonometric.hpp"
 #include "player.hpp"
 
-Camera::Camera(Player& player)
-    : m_player(player), m_horizontal(0.0f), m_vertical(0.0f), m_distance(1.f) {}
-
-Camera::Camera(Player& player, GLfloat horizontal, GLfloat vertical, GLfloat distance)
-    : m_player(player), m_horizontal(horizontal), m_vertical(vertical), m_distance(distance){
-
 Camera::Camera(Player& player, Model& model)
     : m_player(player), m_model(model), distance(0.5f), height(0.4f) {}
 
@@ -20,12 +14,11 @@ void Camera::update(glm::mat4& ViewMatrix)
     glm::mat4 modelRotationMatrix = glm::rotate(glm::mat4(1.0f), m_player.getRotationAngle(), glm::vec3(0.0f, 1.0f, 0.0f));
     // Calcul de la position de la caméra en fonction de la viewMatrix du joueur
     glm::mat4 invPlayerViewMatrix = glm::inverse(ViewMatrix);
-    auto playerPos= glm::vec3(invPlayerViewMatrix[3]); // La position du joueur correspond à la dernière colonne de la viewMatrix
+    auto      playerPos           = glm::vec3(invPlayerViewMatrix[3]); // La position du joueur correspond à la dernière colonne de la viewMatrix
     // glm::vec3 cameraPos = playerPos + distance * glm::vec3(invPlayerViewMatrix[2]) + height * glm::vec3(invPlayerViewMatrix[1]);
 
     glm::vec3 cameraPos = playerPos + distance * glm::vec3(modelRotationMatrix * glm::vec4(invPlayerViewMatrix[2])) + height * glm::vec3(invPlayerViewMatrix[1]);
     // Mise à jour de la vue de la caméra
     ViewMatrix = glm::lookAt(cameraPos, playerPos, glm::vec3(0.0f, 1.0f, 0.0f));
-    m_model.setRotation(m_player.getRotationAngle()); 
+    m_model.setRotation(m_player.getRotationAngle());
 }
-
